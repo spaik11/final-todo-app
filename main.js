8ab4636965e2c2cde15a9dfa07e64d67e19684c6
Sung Paik <sung.paik@codeimmersives.com>
2019-12-14 23:30:57 -0500

    adding display todos function

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const todoList = {
     todos: [],
-    
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!');
+        } else {
+            console.log('My Todos:');
+            for (let i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('( )', this.todos[i].todoText);
+                }
+            }
+        }
+    },
 
 };
