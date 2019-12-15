d215394a582fd54186c1fbebb4d9fa6569ffbf83
Sung Paik <sung.paik@codeimmersives.com>
2019-12-15 00:36:12 -0500

    included functions and event listeners to the new buttons

--- main.js
+++ main.js
@@ -19,7 +19,95 @@ const todoList = {
             todoText: todoText,
             completed: false
         });
-        this.diplayTodos();
+        this.displayTodos();
     },
-    
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        const todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        let totalTodos = this.todos.length;
+        let completedTodos = 0;
+
+        for (let i = 0; i < totalTodos; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+
+        if(completedTodos === totalTodos) {
+            for (let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        } else {
+            for (let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
+    }
 };
+
+const displayTodosButton = document.querySelector('#displayTodosButton');
+const toggleAllButton = document.querySelector('#toggleAll');
+const addTodoTextInput = document.querySelector('#addTodoTextInput');
+const addTodoButton = document.querySelector('#addTodoButton');
+const changeTodoPositionInput = document.querySelector('#changeTodoPositionInput');
+const changeTodoTextInput = document.querySelector('#changeTodoTextInput');
+const changeTodoButton = document.querySelector('#changeTodoButton');
+const deleteTodoButton = document.querySelector('#deleteTodoButton');
+const deleteTodoPositionInput = document.querySelector('#deleteTodoPositionInput');
+const toggleCompletedButton = document.querySelector('#toggleCompletedButton');
+const toggleCompletedPositionInput = document.querySelector('#toggleCompletedPositionInput');
+
+
+displayTodosButton.addEventListener('click', function() {
+    todoList.displayTodos();
+})
+
+toggleAllButton.addEventListener('click', function() {
+    todoList.toggleAll();
+})
+
+addTodoButton.addEventListener('click', function() {
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+})
+
+changeTodoButton.addEventListener('click', function() {
+    todoList.changeTodo(Number(changeTodoPositionInput.value), changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+})
+
+deleteTodoButton.addEventListener('click', function() {
+    todoList.deleteTodo(Number(deleteTodoPositionInput.value))
+    deleteTodoPositionInput.value = '';
+})
+
+toggleCompletedButton.addEventListener('click', function() {
+    todoList.toggleCompleted(Number(toggleCompletedPositionInput.value))
+    toggleCompletedPositionInput.value = '';
+})
+
+const view = {
+    displayTodos: function() {
+        const todosUl = document.querySelector('ul');
+        todosUl.innerHTML = '';
+
+        for (let i = 0; i < todoList.todos.length; i++) {
+            const todoLi = document.createElement('li');
+            todosUl.appendChild(todoLi);
+            
+        }
+    },
+}
